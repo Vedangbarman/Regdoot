02f0305dcb18adacec3f4453d6c0b9c511200dfd
Vedang Barman <vedangbarman@gmail.com>
2026-08-20 17:34:44 +0530

    Creating Repo

---
+++ src/ring.py
@@ -0,0 +1,61 @@
+import os
+import json
+import asyncio
+import pandas as pd
+from datetime import datetime, timezone, timedelta
+from scraper import rbi_webscraper
+
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+out_dir_error_logs = os.path.abspath(os.path.join(script_dir,"..","data","error_logs"))
+os.makedirs(out_dir_error_logs,exist_ok = True)
+
+in_dir_config_file = os.path.abspath(os.path.joi(script_dir,"..","config.json"))
+
+def current_week_file(out_dir,format):
+    today = datetime.now(timezone.utc).date()
+    week_start = today - timedelta(days=today.weekday())
+    if format == "csv":
+        return os.path.join(out_dir, f"{week_start}.csv")
+    elif format == "json":
+        return os.path.join(out_dir, f"{week_start}.json")
+
+async def ring():
+    while True:
+        try:
+            with open(in_dir_config_file) as file:
+                data = json.load(file)
+            time = str(datetime.now())
+            time.strftime("%H")
+            time_scraper = data["time_scraper"]
+            if time_scraper == time:
+                flag = rbi_webscraper()
+                if flag == True:
+                    continue
+                
+            else:
+                pass
+            
+        except Exception as e:
+            print(f"Error {e}")
+            error_message = str(e)
+            count +=1
+            time = str(datetime.now(timezone.utc))
+            errors_ds = {}
+            errors_ds['Error_Message'] = error_message
+            errors_ds['Time'] = time
+            errors_ds['Error Count'] = count
+            errors_ds['Error_File'] = "Scraper"
+                        
+            format_errors = "json"
+            current_path_error_log = current_week_file(out_dir_error_logs,format_errors)
+            data = json.dumps(errors_ds)
+            with open (current_path_error_log, "a") as file:
+                file.write(data + "\n")
+                print(f"Data saved to {current_path_error_log}")
+        
+        sleep_count = 86400
+        n = data["run_count"]
+        await asyncio.sleep(sleep_count/n)
+        
+    
---
+++ src/scraper.py
@@ -0,0 +1,112 @@
+import os
+import lxml
+import json 
+import asyncio
+import requests
+import pandas as pd
+from ring import current_week_file
+from bs4 import BeautifulSoup
+from datetime import datetime, timezone, timedelta
+
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+out_dir_notifications = os.path.abspath(os.path.join(script_dir,"..","data","notifications"))
+os.makedirs(out_dir_notifications,exist_ok = True)
+
+out_dir_error_logs = os.path.abspath(os.path.join(script_dir,"..","data","error_logs"))
+os.makedirs(out_dir_error_logs,exist_ok = True)
+
+      
+        
+        
+def isFileEmpty(filename): 
+    try:
+        if os.stat(filename).st_size > 0:
+               return False
+        else:
+            return True
+    except OSError:
+        print ("No file")
+
+
+
+async def rbi_webscraper():
+    
+    count = 0
+    while count < 5:
+        try: 
+            url = "https://rbi.org.in/notifications_rss.xml"
+            resp.raise_for_status()
+            resp = requests.get(url)
+
+            soup = BeautifulSoup(resp.content, features="xml")
+
+            items = soup.find_all('item')
+
+            pr_items = []
+            
+            format_notifications = "csv"
+            current_path_notifications = current_week_file(out_dir_notifications,format_notifications)
+            for item in items:
+                pr_item = {}
+                pr_item['title'] = item.title.text
+                pr_item['description'] = item.description.text
+                pr_item['link'] = item.link.text
+                pr_item['pubDate'] = item.pubDate.text
+                pr_items.append(pr_item)
+        
+                   
+            pr_dataframe = pd.DataFrame(pr_items,columns=['title','description','link','pubDate'])
+            if pr_items:
+                path_check = os.path.exists(current_path_notifications)
+                if path_check == True:
+                    if isFileEmpty(current_path_notifications) == True:
+                        pr_dataframe.to_csv(current_path_notifications,mode = 'a',header = True,index = False, encoding = 'utf-8')
+                    else:
+                        pr_dataframe.to_csv(current_path_notifications,mode = 'a',header = False,index = False, encoding = 'utf-8')
+                            
+                    print(f"Data saved to {current_path_notifications}")
+                    break
+                else:
+                    pr_dataframe.to_csv(current_path_notifications,mode = 'a',header = True,index = False, encoding = 'utf-8')
+                    break
+            else:
+                break
+            
+            
+        
+        except Exception as e:
+            print(f"Error {e}")
+            error_message = str(e)
+            count +=1
+            time = str(datetime.now(timezone.utc))
+            errors_ds = {}
+            errors_ds['Error_Message'] = error_message
+            errors_ds['Time'] = time
+            errors_ds['Error Count'] = count
+            errors_ds['Error_File'] = "Scraper"
+            
+            format_errors = "json"
+            current_path_error_log = current_week_file(out_dir_error_logs,format_errors)
+            data = json.dumps(errors_ds)
+            with open (current_path_error_log, "a") as file:
+                file.write(data + "\n")
+            print(f"Data saved to {current_path_error_log}")
+        
+                  
+        
+        sleep_count = count*5
+        print(f"Retrying in {sleep_count} seconds.......")
+        await asyncio.sleep(sleep_count)
+        if count == 5:
+            return False
+        else :
+            return True
+        
+if __name__ == "__main__":
+    asyncio.run(rbi_webscraper())
+        
+        
+    
+    

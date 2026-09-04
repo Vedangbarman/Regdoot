b14ea5fbc7386eb33a30903bf397bc351abed40c
Vedang Barman <vedangbarman@gmail.com>
2026-09-04 20:58:39 +0530

    changes

--- sandbox/master_directory_scraper.py
+++ sandbox/master_directory_scraper.py
@@ -1,8 +1,9 @@
 import os 
 import json
+import asyncio
 import requests
+import traceback
 from bs4 import BeautifulSoup
-from utils.week_file_save import current_week_file
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -10,31 +11,60 @@ out_dir_master_directory = os.path.abspath(os.path.join(script_dir,"..","data","
 os.makedirs(out_dir_master_directory,exist_ok = True)
 
 
-def scrape_master_directory():
+async def scrape_master_directory():
     intial_id = 12931
-    
-    for i in range(0,35):
-        id = intial_id + i
-        url = f"https://rbi.org.in/scripts/BS_ViewMasDirections.aspx?id={id}"
-        resp = requests.get(url)
-        resp.raise_for_status()
-        soup = BeautifulSoup(resp.content, features="html.parser")
-        
-        title = soup.find('b').get_text(strip=True)
-        
-        
-        content = soup.find('tr', class_='tablecontent2')
-        for junk in content.find_all(["script", "style", "input"]):
-            junk.decompose()
-        clean_text = content.get_text(separator="\n", strip=True).replace("\r", "")
-        
-        clean_data = {"id": id, "title": title, "category": "NBFC", "url": url, "text": clean_text}
-        
-        file_path = os.path.abspath(os.path.join(out_dir_master_directory,f"{id}.json"))
-        with open(file_path,"w",encoding = "utf-8") as f:
-            json.dump(clean_data,f)
-        
-        
-        
-scrape_master_directory()
+    second_id = 13585
+    file_path = os.path.abspath(os.path.join(out_dir_master_directory,f"master_directory.jsonl"))
+    count = 0
+    i = 0 
+    while True:
+        try:
+            for i in range(0,42):
+                if i <= 34:
+                    id = intial_id + i
+                    url = f"https://rbi.org.in/scripts/BS_ViewMasDirections.aspx?id={id}"
+                    resp = requests.get(url)
+                    resp.raise_for_status()
+                    soup = BeautifulSoup(resp.content, features="html.parser")
+                    
+                    title = soup.find('b').get_text(strip=True)
+                    
+                    
+                    content = soup.find('tr', class_='tablecontent2')
+                    for junk in content.find_all(["script", "style", "input"]):
+                        junk.decompose()
+                    clean_text = content.get_text(separator="\n", strip=True).replace("\r", "")
+                    
+                    clean_data = {"id": id, "title": title, "category": "NBFC", "url": url, "text": clean_text}
+                    with open(file_path,"a",encoding = "utf-8") as f:
+                        f.write(json.dumps(clean_data) + "\n")
+                
+                elif i > 34:
+                    id = second_id + i
+                    url = f"https://rbi.org.in/scripts/BS_ViewMasDirections.aspx?id={id}"
+                    resp = requests.get(url)
+                    resp.raise_for_status()
+                    soup = BeautifulSoup(resp.content, features="html.parser")
+                    title = soup.find('b').get_text(strip=True)
+                    content = soup.find('tr', class_='tablecontent2')
+                    for junk in content.find_all(["script", "style", "input"]):
+                        junk.decompose()
+                    clean_text = content.get_text(separator="\n", strip=True).replace("\r", "")
+                    clean_data = {"id": id, "title": title, "category": "NBFC", "url": url, "text": clean_text}
+                    
+                    with open(file_path,"a",encoding = "utf-8") as f:
+                        f.write(json.dumps(clean_data )+ "\n")
+                elif i == 41:
+                    break
+        except Exception as e:
+            if count < 25:
+                traceback.print_exc()
+                count +=5
+                await asyncio.sleep(count)
+            else:
+                break
+            
+            
+if __name__ == "__main__": 
+    asyncio.run(scrape_master_directory())
         

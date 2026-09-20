784a021fd942ffa3c0755705af1e2ade5adea2bb
Vedang Barman <vedangbarman@gmail.com>
2026-09-20 23:46:09 +0530

    pipeline testing with model api

---
+++ src/ai_inference.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_google_genai import ChatGoogleGenerativeAI
+load_dotenv()
+
+API_KEY = os.getenv('GEMINI')
+
+
+os.environ["GOOGLE_API_KEY"] = API_KEY
+
+model = ChatGoogleGenerativeAI(model = "gemini-3.7-flash")
+
+
+response = model.invoke("Hello, Gemini!")
+print(response.content)
